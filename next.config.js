8d3fd878a38eb5037241275c277f57f2f2a229c7
Robin Escobar <rescobar@buildwithin.com>
2025-04-21 22:20:15 -0600

    turn on placehold.co to use svg

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['placehold.co'],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
+
+module.exports = nextConfig; 
